c97b587db1380377b8b5dd55097274736e9ae483
Miles <miles.moscara@gmail.com>
2019-04-22 10:42:38 -0400

    logic updates

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,77 @@
-let wins = 0;
-let losses = 0;
-let targetnum = 0;
+
+
+
+
+
+
+
+
+let bluecrystal = Math.floor(Math.random() * 10) + 1;
+let redcrystal = Math.floor(Math.random() * 10) + 1;
+let greencrystal = Math.floor(Math.random() * 10) + 1;
+let pinkcrystal = Math.floor(Math.random() * 10) + 1;
 let runtotal = 0;
-let isGamecomplete = true;
+const targetnum = Math.floor(Math.random() * 40) + 5;
+let wins = 0;
+
+
+
+
+$(function () {
+
+
+
+
+    $("#bluecrystal").click(function () {
+        runtotal += bluecrystal;
+        $("#runtotal").text(runtotal);
+        $("#targetnum").text(targetnum);
+
+        if ((runtotal === targetnum) && (runtotal != 0)) {
+            wins++;
+        }
+
+        $("#wins").text(wins);
+
+    });
+
+    $("#redcrystal").click(function () {
+        runtotal += redcrystal;
+        $("#runtotal").text(runtotal);
+        $("#targetnum").text(targetnum);
+
+        if ((runtotal === targetnum) && (runtotal != 0)) {
+            wins++;
+        }
+        $("#wins").text(wins);
+
+
+    });
+
+    $("#greencrystal").click(function () {
+        runtotal += greencrystal;
+        $("#runtotal").text(runtotal);
+        $("#targetnum").text(targetnum);
+
+        if ((runtotal === targetnum) && (runtotal != 0)) {
+            wins++;
+        }
+        $("#wins").text(wins);
+
+    });
+
+    $("#pinkcrystal").click(function () {
+        runtotal += pinkcrystal;
+        $("#runtotal").text(runtotal);
+        $("#targetnum").text(targetnum);
+
+        if ((runtotal === targetnum) && (runtotal != 0)) {
+            wins++;
+        }
+        $("#wins").text(wins);
+
+
+    });
+
+});
+
